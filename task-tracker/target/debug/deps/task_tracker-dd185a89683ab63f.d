b5c7dfa7a97b3d85799c9f0b23879f2ecefae41d
Jace Rodgers <jaransas2008@gmail.com>
2026-03-26 11:41:05 -0500

    for got the other search bars

---
+++ task-tracker/target/debug/deps/task_tracker-dd185a89683ab63f.d
@@ -0,0 +1,5 @@
+/Users/jacerodgers/Code/JHL/task-tracker/target/debug/deps/task_tracker-dd185a89683ab63f.d: src/main.rs
+
+/Users/jacerodgers/Code/JHL/task-tracker/target/debug/deps/libtask_tracker-dd185a89683ab63f.rmeta: src/main.rs
+
+src/main.rs:
---
+++ task-tracker/target/debug/deps/task_tracker-e81d2590d3564368.d
@@ -0,0 +1,5 @@
+/Users/jacerodgers/Code/JHL/task-tracker/target/debug/deps/task_tracker-e81d2590d3564368.d: src/main.rs
+
+/Users/jacerodgers/Code/JHL/task-tracker/target/debug/deps/libtask_tracker-e81d2590d3564368.rmeta: src/main.rs
+
+src/main.rs:
